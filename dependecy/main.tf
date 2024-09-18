2a98df674c38db5ec895bc54b4f6323fe295c4bc
Ubuntu <ubuntu@ip-172-31-5-58.ap-south-1.compute.internal>
2024-09-18 17:31:50 +0000

    explicit dependency

--- dependecy/main.tf
+++ dependecy/main.tf
@@ -3,12 +3,19 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_eip" "ip" {
-  instance = aws_instance.example.id
-}
+#commenting for now used for implicity dependency
+# resource "aws_eip" "ip" {
+#  instance = aws_instance.example.id
+# }
 
 resource "aws_instance" "example" {
   ami           = "ami-0ad21ae1d0696ad58"
   instance_type = "t2.micro"
+  depends_on    = [aws_s3_bucket.example]
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "sept202418kum2"
+
 }
 
