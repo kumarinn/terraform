83890f707816c3c81e1fef6ac2e4a6d4443e8aee
Ubuntu <ubuntu@ip-172-31-5-58.ap-south-1.compute.internal>
2024-09-21 07:17:21 +0000

    aws webserver

--- nginx-using-docker/main.tf
+++ nginx-using-docker/main.tf
@@ -2,24 +2,24 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
-}
-}
+    }
+  }
 }
 
 provider "docker" {}
 
-resource "docker_image" "nginx"  {  #downloads the nginx image from the dockerhub 
-  name = "nginx"
+resource "docker_image" "nginx" { #downloads the nginx image from the dockerhub 
+  name         = "nginx"
   keep_locally = "false"
 }
 
 
 resource "docker_container" "nginx" {
-  image = docker_image.image_name  #implicit dependency ( Container depends on the image) Container - Child block, Image: Parent Block
-  name = "nginx"
+  image = docker_image.nginx.image_id #implicit dependency ( Container depends on the image) Container - Child block, Image: Parent Block
+  name  = "nginx"
   ports {
     internal = 80   #nginx default port i.e container port
-    external = 8000  # host port
-}
+    external = 8000 # host port
+  }
 }
 
