8c08e12333bdd377620d76b418e2fb07354e6506
Ubuntu <ubuntu@ip-172-31-5-58.ap-south-1.compute.internal>
2024-09-22 17:52:52 +0000

    overriding the child module variables in root module by using variables file and in command line

--- rootmod/main.tf
+++ rootmod/main.tf
@@ -1,3 +1,5 @@
 module "instance" {
   source = "./modules/instance"
+  amid = var.amid
+  type = var.type
 }
---
+++ rootmod/variables.tf
@@ -0,0 +1,7 @@
+variable "amid" {
+  default = "ami-0522ab6e1ddcc7055"
+}
+
+variable "type" {
+  default = "t2.micro"
+}
